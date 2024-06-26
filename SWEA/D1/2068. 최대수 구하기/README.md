64ad18cb1b8d58cd399451b459f308af0fffd93b
gyuchan <83599750+gyudol@users.noreply.github.com>
2024-06-26 15:48:59 +0900

    [D1] Title: 최대수 구하기, Time: 104 ms, Memory: 23,236 KB -BaekjoonHub

---
+++ SWEA/D1/2068. 최대수 구하기/README.md
@@ -0,0 +1,15 @@
+# [D1] 최대수 구하기 - 2068 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QQhbqA4QDFAUq) 
+
+### 성능 요약
+
+메모리: 23,236 KB, 시간: 104 ms, 코드길이: 756 Bytes
+
+### 제출 일자
+
+2024-06-26 15:48
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	private static final int LEN = 10;
+	
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder result = new StringBuilder();
+		
+		for(int tc = 1; tc <= T; tc++) {
+			int max = Integer.MIN_VALUE;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			for(int i = 0; i < LEN; i++) {
+				int num = Integer.parseInt(st.nextToken());
+				
+				if(num > max) max = num;
+			}
+			
+			result.append('#').append(tc).append(' ').append(max).append('\n');
+		}
+		
+		System.out.print(result);
+	}
+}
