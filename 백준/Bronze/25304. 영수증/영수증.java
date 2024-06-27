6347bacabe69005a4e7b706b36e279a08a2041b9
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-06-28 02:20:29 +0900

    [Bronze IV] Title: 영수증, Time: 76 ms, Memory: 11608 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -3,7 +3,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
 	public static void main(String [] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int total = Integer.parseInt(br.readLine());
