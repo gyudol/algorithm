64b8ce27ab6a33761e10db8748400dbf2fac4e33
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-06-28 02:14:32 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 64 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder result = new StringBuilder();
+		
+		for(int i = 0; i < n / 4; i++) 
+			result.append('l').append('o').append('n').append('g').append(" ");
+		result.append('i').append('n').append('t');
+
+		System.out.print(result);
+	}
+}
