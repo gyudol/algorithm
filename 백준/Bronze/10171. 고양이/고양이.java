7b47e572b0092202fff8c1a3bd3763ffc25671bd
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-06-27 23:55:01 +0900

    [Bronze V] Title: 고양이, Time: 72 ms, Memory: 11232 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String [] args) throws Exception {
+		StringBuilder result = new StringBuilder();
+		
+		result.append("\\    /\\").append('\n')
+			.append(" )  ( ')").append('\n')
+			.append("(  /  )").append('\n')
+			.append(" \\(__)|");
+		
+		System.out.print(result);
+	}
+}
