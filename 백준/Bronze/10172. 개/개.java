560347576d204e166278e5f995f78da169a9bb80
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-06-27 23:57:38 +0900

    [Bronze V] Title: 개, Time: 72 ms, Memory: 11316 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String [] args) throws Exception {
+		StringBuilder result = new StringBuilder();
+		
+		result.append("|\\_/|").append('\n')
+			.append("|q p|   /}").append('\n')
+			.append("( 0 )\"\"\"\\").append('\n')
+			.append("|\"^\"`    |").append('\n')
+			.append("||_/=\\\\__|");
+		
+		System.out.print(result);
+	}
+}
