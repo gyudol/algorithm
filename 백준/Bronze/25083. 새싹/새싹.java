70139fd18ff16305e8276b4f9f52e36528e99895
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-06-28 16:13:11 +0900

    [Bronze V] Title: 새싹, Time: 76 ms, Memory: 11352 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String [] args) throws Exception {
+		StringBuilder result = new StringBuilder();
+		
+		result.append("         ,r'\"7").append('\n')
+			.append("r`-_   ,'  ,/").append('\n')
+			.append(" \\. \". L_r'").append('\n')
+			.append("   `~\\/").append('\n')
+			.append("      |").append('\n')
+			.append("      |");
+		
+		System.out.print(result);
+	}
+}
