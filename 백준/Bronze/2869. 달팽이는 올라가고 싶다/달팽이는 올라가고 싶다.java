6259ad2afc8b19148ebb9af9f065ea503e90f5b1
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-06-30 03:53:58 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 68 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -8,9 +8,8 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()),
 				v = Integer.parseInt(st.nextToken());
-		int target = v - a >= 0 ? v - a : 0;	// 마지막 날 제외
-		int result = (int) Math.ceil(target * 1.0 / (a - b)) + (target >= 0 ? 1 : 0);
+		int target = v - a;	// 마지막 날 제외 (v - a >= 0)
 		
-		System.out.print(result);
+		System.out.print((int) Math.ceil(target * 1.0 / (a - b)) + 1);
 	}
 }
