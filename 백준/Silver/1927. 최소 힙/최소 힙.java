88349f2db86cb662efb6707e055482abf843b688
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-06-30 15:31:30 +0900

    [Silver II] Title: 최소 힙, Time: 248 ms, Memory: 24776 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder result = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		for(int i = 0; i < n; i++) {
+			int x = Integer.parseInt(br.readLine());
+			
+			if(x == 0) result.append(pq.isEmpty() ? 0 : pq.poll()).append('\n');
+			else pq.offer(x);
+		}
+		
+		System.out.print(result);
+	}
+}
