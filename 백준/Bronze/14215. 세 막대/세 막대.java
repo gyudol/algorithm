97c970681f5d01581cb6cf8b74b6c652d8f48de4
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-06-30 21:25:15 +0900

    [Bronze III] Title: 세 막대, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static final int LEN = 3;
+	
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int [] tri = new int [LEN];
+		
+		for(int i = 0; i < LEN; i++) tri[i] = Integer.parseInt(st.nextToken());
+		Arrays.sort(tri);
+		
+		while(tri[0] + tri[1] <= tri[2]) {
+			tri[2]--;
+			Arrays.sort(tri);
+		}
+		
+		System.out.print(tri[0] + tri[1] + tri[2]);
+	}
+}
