5032bd84f51ebc296a28127c8e054ce5f1455544
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-07-02 03:04:12 +0900

    [Bronze V] Title: 등록, Time: 56 ms, Memory: 11156 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String [] args) throws Exception {
+		StringBuilder result = new StringBuilder();
+
+		result.append(202).append('\n').append('p').append('p').append('p')
+			.append(3408);
+		
+		System.out.print(result);
+	}
+}
