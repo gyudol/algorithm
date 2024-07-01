8bd938e8d9c48a7fe26dda5ed8f0285e55828994
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-07-02 01:23:02 +0900

    [Bronze II] Title: 음계, Time: 76 ms, Memory: 11416 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static final int LEN = 8;
+	private static final int [] ASC_SCALE = {1, 2, 3, 4, 5, 6, 7, 8};
+	
+	private static String perform(int [] scale) {
+		int ascCnt = 0, desCnt = 0;
+		
+		for(int i = 0; i < LEN; i++) {
+			if(scale[i] == ASC_SCALE[i]) ascCnt++;
+			else if(scale[i] == ASC_SCALE[LEN - 1 - i]) desCnt++;
+			else return "mixed";
+		}
+		
+		return ascCnt == LEN ? "ascending" : desCnt == LEN ? "descending" : "mixed";
+	}
+	
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int [] scale = new int [LEN];
+		
+		for(int i = 0; i < LEN; i++) scale[i] = Integer.parseInt(st.nextToken());
+		
+		System.out.print(perform(scale));
+	}
+}
