6581217f11bceee69759e7344fa5ff72386f44b2
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-07-01 16:19:51 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 68 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -4,13 +4,9 @@ import java.util.StringTokenizer;
 
 public class Main {
 	private static int isSatisfied(int a1, int a0, int c, int n0) {
-		// a1 * n0 + a0 <= c * n0
-		if(a1 > c || (a1 == c && a0 > 0)) return 0;
-		else if(a1 == c) return 1;
-		else {	// a1 < c
-			if(a1 * n0 + a0 <= c * n0) return 1;	// 최소값 n0만 만족하면 만족
-			return 0;
-		}
+		// 최솟값 n0에 대해서만 만족하면 식이 성립
+		if(a1 * n0 + a0 <= c * n0 && c >= a1) return 1;
+		return 0;
 	}
 	
 	public static void main(String [] args) throws Exception {
