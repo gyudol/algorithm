02fdb0cb234a08f466be6789c3a2e887e77ef18e
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-07-02 02:51:57 +0900

    [Silver V] Title: 나이순 정렬, Time: 460 ms, Memory: 46396 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,29 +1,40 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Main {
+	private static class Member implements Comparable<Member> {
+		public int age;
+		public String name;
+		
+		private Member(int age, String name) {
+			this.age = age;
+			this.name = name;
+		}
+		
+		@Override
+		public int compareTo(Member other) {
+			return Integer.compare(age, other.age);
+		}
+	}
+	
 	public static void main(String [] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder result = new StringBuilder();
 		int n = Integer.parseInt(br.readLine());
-		String [][] members = new String [n][2];
+		Member [] members = new Member [n];
 		
-		for(int i = 0; i < n; i++) members[i] = br.readLine().split(" ");
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			members[i] = new Member(Integer.parseInt(st.nextToken()), st.nextToken());
+		}
 		
-		Arrays.sort(members, new Comparator<String []>() {
-			@Override
-			public int compare(String [] s1, String [] s2) {
-				int age1 = Integer.parseInt(s1[0]),
-						age2 = Integer.parseInt(s2[0]);
-				
-				return Integer.compare(age1, age2);
-			}
-		});
+		Arrays.sort(members);
 		
-		for(String [] m : members) 
-			result.append(m[0]).append(' ').append(m[1]).append('\n');
+		for(Member m : members) 
+			result.append(m.age).append(' ').append(m.name).append('\n');
 		
 		System.out.print(result);
 	}
