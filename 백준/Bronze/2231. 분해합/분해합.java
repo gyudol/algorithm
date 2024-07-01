ee261d5444052e1cc823d1325c0e83e90e422ce1
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-07-01 17:23:26 +0900

    [Bronze II] Title: 분해합, Time: 88 ms, Memory: 11500 KB

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 	private static int decompSum(int n) {
@@ -24,8 +23,7 @@ public class Main {
 	
 	public static void main(String [] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(br.readLine());
 		
 		System.out.print(constructor(n));
 	}
