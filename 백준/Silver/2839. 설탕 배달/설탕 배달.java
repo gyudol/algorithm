825f0a4ff0c7ed68758bfc8f09b708fe12b17e0a
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-07-01 22:44:58 +0900

    [Silver IV] Title: 설탕 배달, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	private static final int MAX = 1667;	// (4995 / 3) + (5 / 1) = 1666
+	private static final int [] ENV = {3, 5};
+	private static int [] dp;
+	
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		dp = new int [n + 1];
+		
+		Arrays.fill(dp, MAX);
+		dp[0] = 0;
+		
+		for(int i = 0; i <= n; i++) {
+			for(int env : ENV) {
+				if(i >= env) dp[i] = Math.min(dp[i], dp[i - env] + 1);
+			}
+		}
+		
+		System.out.print(dp[n] == MAX ? -1 : dp[n]);
+	}
+}
