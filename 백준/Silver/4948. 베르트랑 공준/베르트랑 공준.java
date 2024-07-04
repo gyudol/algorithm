2aa3532a022560dd87df9c7e3b98a071023856d9
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-07-04 14:29:24 +0900

    [Silver II] Title: 베르트랑 공준, Time: 116 ms, Memory: 12088 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	private static final int MAX = 123456 * 2;
+	
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder result = new StringBuilder();
+		boolean [] isNotPrime = new boolean [MAX + 1];
+		isNotPrime[0] = isNotPrime[1] = true;
+		
+		for(int i = 2; i < isNotPrime.length; i++) {
+			if(isNotPrime[i]) continue;
+			
+			for(long j = i * 1L * i; j < isNotPrime.length; j += i) isNotPrime[(int) j] = true;
+		}
+		
+		while(true) {
+			int n = Integer.parseInt(br.readLine());
+			int cnt = 0;
+			
+			if(n == 0) break;
+			
+			for(int i = n + 1; i <= n * 2; i++) {
+				if(!isNotPrime[i]) cnt++;
+			}
+			
+			result.append(cnt).append('\n');
+		}
+		
+		System.out.print(result);
+	}
+}
