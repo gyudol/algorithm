5ec6f3f15dff5cc17db0d5f9ee2419743160d919
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-07-04 14:07:35 +0900

    [Silver III] Title: 소수 구하기, Time: 176 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder result = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int m = Integer.parseInt(st.nextToken()), n = Integer.parseInt(st.nextToken());
+		boolean [] isNotPrime = new boolean [n + 1];
+		isNotPrime[0] = isNotPrime[1] = true;
+		
+		for(int i = 2; i <= n; i++) {
+			if(isNotPrime[i]) continue;
+			
+			for(long j = i * 1L * i; j <= n; j += i) isNotPrime[(int) j] = true;
+		}
+		
+		for(int i = m; i <= n; i++) {
+			if(!isNotPrime[i]) result.append(i).append('\n');
+		}
+		
+		System.out.print(result);
+	}
+}
