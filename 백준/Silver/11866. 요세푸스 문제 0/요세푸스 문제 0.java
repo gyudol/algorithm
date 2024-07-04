db2ee362f3c10221110c23c52f5e33eada586788
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-07-04 18:09:08 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 72 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -3,23 +3,24 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Main {
 	public static void main(String [] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder result = new StringBuilder().append('<');
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken()) - 1;
 		int cur = 0;
-		List<Integer> circle = IntStream.range(1, n + 1).boxed().collect(Collectors.toList());
 		List<Integer> josephus = new ArrayList<>();
 		
-		while(!circle.isEmpty()) {
-			cur = (cur + k) % circle.size();
-			josephus.add(circle.remove(cur));
-		}
+		for(int i = 1; i <= n; i++) josephus.add(i);
 		
-		System.out.print(josephus.toString().replace("[", "<").replace("]", ">"));
+		while(josephus.size() > 1) {
+			cur = (cur + k) % josephus.size();
+			result.append(josephus.remove(cur)).append(',').append(' ');
+		}
+		result.append(josephus.remove(0)).append('>');
+		
+		System.out.print(result);
 	}
 }
