27071bf492f485537a43ac313d59347c60ed812e
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-07-05 17:36:33 +0900

    [Silver II] Title: 나무 자르기, Time: 664 ms, Memory: 175160 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -8,23 +8,25 @@ public class Main {
 	private static int m;
 	private static int [] trees;
 	
-	private static long woodCutting(long height) {
+	private static long woodCutting(int height) {
 		long sum = 0;
 		
 		for(int i = n - 1; i >= 0; i--) {
-			if(trees[i] - height <= 0) break;
+			int log = trees[i] - height;
 			
-			sum += trees[i] - height;
+			if(log <= 0) break;
+			
+			sum += log;
 		}
 			
 		return sum;
 	}
 	
-	private static long binarySearch() {
-		long start = 0, end = trees[n - 1];	// [start, end)
+	private static int binarySearch() {
+		int start = 0, end = trees[n - 1];	// [start, end)
 		
 		while(end - start > 1) {
-			long mid = (start + end) / 2;
+			int mid = (start + end) / 2;
 			
 			if(woodCutting(mid) >= m) start = mid;
 			else end = mid;
