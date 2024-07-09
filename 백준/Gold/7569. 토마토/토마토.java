191141b8870a68d81bd3f653b661303f721eacdd
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-07-09 19:16:58 +0900

    [Gold V] Title: 토마토, Time: 644 ms, Memory: 131628 KB -BaekjoonHub

---
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static final int [][] DIR = 
+		{{0, 0, 1}, {0, 0, -1}, {0, -1, 0}, {0, 1, 0}, {1, 0, 0}, {-1, 0, 0}};
+	private static int [][][] tomatoes;
+	private static Queue<Tomato> ripeTomatoes;
+	private static boolean [][][] isVisited;
+	private static int N, M, H;
+	
+	private static class Tomato {
+		public int row;
+		public int col;
+		public int height;
+		public int day;
+		
+		private Tomato(int row, int col, int height, int day) {
+			this.row = row;
+			this.col = col;
+			this.height = height;
+			this.day = day;
+		}
+	}
+	
+	private static int bfs(int raw) {
+		if(raw == 0) return 0;
+		
+		while(!ripeTomatoes.isEmpty()) {
+			Tomato tomato = ripeTomatoes.poll();
+			int nextDay = tomato.day + 1;
+			
+			for(int [] d : DIR) {
+				int nextRow = tomato.row + d[0];
+				int nextCol = tomato.col + d[1];
+				int nextHeight = tomato.height + d[2];
+				
+				if(nextRow < 0 || nextRow >= N || nextCol < 0 || nextCol >= M
+						|| nextHeight < 0 || nextHeight >= H || isVisited[nextRow][nextCol][nextHeight]
+								|| tomatoes[nextRow][nextCol][nextHeight] == -1) continue;
+				
+				if(--raw == 0) return nextDay;
+				isVisited[nextRow][nextCol][nextHeight] = true;
+				ripeTomatoes.offer(new Tomato(nextRow, nextCol, nextHeight, nextDay));
+			}
+		}
+		
+		return -1;
+	}
+	
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken()); N = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+		int raw = 0;
+		tomatoes = new int [N][M][H];
+		ripeTomatoes = new LinkedList<>();
+		isVisited = new boolean [N][M][H];
+		
+		for(int h = 0; h < H; h++) {
+			for(int n = 0; n < N; n++) {
+				st = new StringTokenizer(br.readLine());
+				
+				for(int m = 0; m < M; m++) {
+					tomatoes[n][m][h] = Integer.parseInt(st.nextToken());
+					
+					if(tomatoes[n][m][h] == 1) {
+						isVisited[n][m][h] = true;
+						ripeTomatoes.offer(new Tomato(n, m, h, 0));
+					}
+					else if(tomatoes[n][m][h] == 0) raw++;
+				}
+			}
+		}
+		
+		System.out.print(bfs(raw));
+	}
+}
