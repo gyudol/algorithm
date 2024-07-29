a01a5491dd59223eadbc75a71dfc41d915b2d5be
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-07-29 13:40:42 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 80 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,39 +1,36 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static final char [] SYMBOLS = {'+', '-', '*', '/'};
+	private static final int LEN = 4;
 	private static int n;
+	private static int [] a;
+	private static int [] operators;
+	private static int min;
+	private static int max;
 	
-	private static void buildOperatorSet(Set<String> operatorSet, String prefix, int [] items) {
-		if(prefix.length() == n - 1) {
-			operatorSet.add(prefix);
+	private static void dfs(int value, int depth) {
+		if(depth == n) {
+			min = Math.min(value, min);
+			max = Math.max(value, max);
+			
 			return;
 		}
 		
-		for(int i = 0; i < items.length; i++) {
-			if(items[i] == 0) continue;
+		for(int i = 0; i < LEN; i++) {
+			if(operators[i] == 0) continue;
+			operators[i]--;
 			
-			items[i]--;
-			buildOperatorSet(operatorSet, prefix + SYMBOLS[i], items);
-			items[i]++;
+			switch(i) {
+				case 0: dfs(value + a[depth], depth + 1); break;
+				case 1: dfs(value - a[depth], depth + 1); break;
+				case 2: dfs(value * a[depth], depth + 1); break;
+				case 3: dfs(value / a[depth], depth + 1); break;
+			}
+			
+			operators[i]++;
 		}
-	}
-	
-	private static int calculate(int a, char operator, int b) {
-		int value = -1;
-		
-		switch(operator) {
-			case '+': value = a + b;	break;
-			case '-': value = a - b;	break;
-			case '*': value = a * b;	break;
-			case '/': value = a / b;	break;
-		}
-		
-		return value;
 	}
 	
 	public static void main(String [] args) throws Exception {
@@ -41,25 +38,16 @@ public class Main {
 		StringBuilder result = new StringBuilder();
 		n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int [] a = new int [n];
-		int [] items = new int [SYMBOLS.length];
-		int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+		a = new int [n];
+		operators = new int [LEN];
+		min = Integer.MAX_VALUE;
+		max = Integer.MIN_VALUE;
 		
 		for(int i = 0; i < a.length; i++) a[i] = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < items.length; i++) items[i] = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < LEN; i++) operators[i] = Integer.parseInt(st.nextToken());
 		
-		Set<String> operatorSet = new HashSet<>();
-		buildOperatorSet(operatorSet, "", items);
-		
-		for(String operators : operatorSet) {
-			int value = a[0];
-			
-			for(int i = 1; i < n; i++) value = calculate(value, operators.charAt(i - 1), a[i]);
-			
-			if(value < min) min = value;
-			if(value > max) max = value;
-		}
+		dfs(a[0], 1);
 		
 		result.append(max).append('\n').append(min);
 		
