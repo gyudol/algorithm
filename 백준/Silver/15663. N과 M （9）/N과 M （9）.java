8523c388a4f21a98aef7c5854510211603053dc8
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-08-11 11:43:48 +0900

    [Silver II] Title: N과 M (9), Time: 96 ms, Memory: 16908 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.java
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,23 +9,23 @@ public class Main {
 	private static int m;
 	private static int [] numbers;
 	
-	private static void dfs(int depth, String seq, boolean [] isVisitedIdx, Set<String> isVisitedSeq) {
+	private static void dfs(int depth, int [] seq, boolean [] isVisited) {
 		if(depth == m) {
-			if(isVisitedSeq.contains(seq)) return;
-			isVisitedSeq.add(seq);
-			
-			result.append(seq).append('\n');
+			for(int num : seq) result.append(num).append(' ');
+			result.append('\n');
 			
 			return;
 		}
 		
+		int before = 0;	// 동일 depth에서 같은 값을 선택 못하게
 		for(int i = 0; i < n; i++) {
-			if(isVisitedIdx[i]) continue;
-			isVisitedIdx[i] = true;
+			if(isVisited[i] || before == numbers[i]) continue;
+			isVisited[i] = true;
+			seq[depth] = before = numbers[i];
 			
-			dfs(depth + 1, seq + numbers[i] + ' ', isVisitedIdx, isVisitedSeq);
+			dfs(depth + 1, seq, isVisited);
 
-			isVisitedIdx[i] = false;
+			isVisited[i] = false;
 		}
 	}
 	
@@ -42,7 +40,7 @@ public class Main {
 		for(int i = 0; i < n; i++) numbers[i] = Integer.parseInt(st.nextToken());
 		Arrays.sort(numbers);
 		
-		dfs(0, "", new boolean [n], new HashSet<>());
+		dfs(0, new int [m], new boolean [n]);
 		
 		System.out.print(result);
 	}
