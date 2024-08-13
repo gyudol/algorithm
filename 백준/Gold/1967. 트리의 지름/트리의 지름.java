c5fc8154202805088a025a888aae9fe8ffca2416
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-08-13 19:35:39 +0900

    [Gold IV] Title: 트리의 지름, Time: 188 ms, Memory: 20484 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,12 +1,12 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	private static int n;
+	private static int maxWeight;
+	private static int maxVertex;
 	private static ArrayList<ArrayList<Node>> tree;
 	
 	private static class Node implements Comparable<Node> {
@@ -24,47 +24,23 @@ public class Main {
 		}
 	}
 	
-	private static int dijkstra(int start, boolean isDiameterRequired) {
-		int [] weights = new int [n + 1];
-		Arrays.fill(weights, Integer.MAX_VALUE);
-		weights[start] = 0;
+	private static void dfs(int src, int sum, boolean [] isVisited) {
+		if(isVisited[src]) return;
+		isVisited[src] = true;
 		
-		int maxWeight = Integer.MIN_VALUE, maxVertex = -1;
-		
-		PriorityQueue<Node> minHeap = new PriorityQueue<>();
-		minHeap.offer(new Node(start, 0));
-		
-		while(!minHeap.isEmpty()) {
-			Node node = minHeap.poll();
-			int src = node.vertex;
-			int weight = node.weight;
-			
-			if(weight > weights[src]) continue;
-			
-			for(Node nextNode : tree.get(src)) {
-				int des = nextNode.vertex;
-				int nextWeight = weight + nextNode.weight;
-				
-				if(nextWeight < weights[des]) {
-					weights[des] = nextWeight;
-					minHeap.offer(new Node(des, nextWeight));
-				}
-			}
+		if(sum > maxWeight) {
+			maxWeight = sum;
+			maxVertex = src;
 		}
 		
-		for(int i = 1; i <= n; i++) {
-			if(weights[i] > maxWeight) {
-				maxWeight = weights[i];
-				maxVertex = i;
-			}
-		}
-		
-		return isDiameterRequired ? maxWeight : maxVertex;
+		for(Node node : tree.get(src)) dfs(node.vertex, sum + node.weight, isVisited);
 	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
+		maxWeight = Integer.MIN_VALUE;
+		maxVertex = -1; 
 		tree = new ArrayList<>();
 		
 		for(int i = 0; i <= n; i++) tree.add(new ArrayList<>());
@@ -78,8 +54,10 @@ public class Main {
 			tree.get(des).add(new Node(src, weight));
 		}
 		
-		int diameterPoint = dijkstra(1, false);
+		dfs(1, 0, new boolean [n + 1]);
+		maxWeight = Integer.MIN_VALUE;
+		dfs(maxVertex, 0, new boolean [n + 1]);
 		
-		System.out.print(dijkstra(diameterPoint, true));
+		System.out.print(maxWeight);
 	}
 }
