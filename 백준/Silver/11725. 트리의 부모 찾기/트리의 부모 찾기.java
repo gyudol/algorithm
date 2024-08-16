aba81c25ea3d4aa2fd1b18d815891bd697695171
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-08-16 14:58:51 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 432 ms, Memory: 75564 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static int n;
+	private static int [] tree;		// element = parent
+	private static ArrayList<ArrayList<Integer>> edges;
+	
+	private static void dfs(int src, boolean [] isVisited) {
+		for(int des : edges.get(src)) {
+			if(isVisited[des]) continue;
+			
+			isVisited[des] = true;
+			tree[des] = src;
+			
+			dfs(des, isVisited);
+		}
+	}
+	
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder result = new StringBuilder();
+		n = Integer.parseInt(br.readLine());
+		tree = new int [n + 1];
+		edges = new ArrayList<>();
+		
+		for(int i = 0; i <= n; i++) edges.add(new ArrayList<>());
+		for(int i = 0; i < n - 1; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n1 = Integer.parseInt(st.nextToken()), n2 = Integer.parseInt(st.nextToken());
+			
+			edges.get(n1).add(n2);
+			edges.get(n2).add(n1);
+		}
+		
+		dfs(1, new boolean [n + 1]);
+		for(int i = 2; i <= n; i++) result.append(tree[i]).append('\n');
+		
+		System.out.print(result);
+	}
+}
