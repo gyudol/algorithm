76f8b815892c44f66af70b3c7bac5db882308447
Gyuchan Kim <sb3g@naver.com>
2024-08-24 16:10:16 +0900

    [Gold II] Title: 피보나치 수 6, Time: 64 ms, Memory: 11512 KB

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
@@ -4,10 +4,11 @@ import java.io.InputStreamReader;
 public class Main {
 	private static final int LEN = 2;
 	private static final int MOD = 1000000007;
-	private static final long [][] initialMatrix = {{1, 1}, {1, 0}};
+	private static final long[][] UNIT_MATRIX = {{1, 0}, {0, 1}};
+	private static final long[][] INITIAL_MATRIX = {{1, 1}, {1, 0}};
 	
-	private static long [][] multiply(long [][] o1, long [][] o2) {
-		long [][] res = new long [LEN][LEN];
+	private static long[][] multiply(long[][] o1, long[][] o2) {
+		long[][] res = new long[LEN][LEN];
 		
 		res[0][0] = (o1[0][0] * o2[0][0] + o1[0][1] * o2[1][0]) % MOD;
 		res[0][1] = (o1[0][0] * o2[0][1] + o1[0][1] * o2[1][1]) % MOD;
@@ -17,32 +18,27 @@ public class Main {
 		return res;
 	}
 	
-	private static long [][] pow(long [][] a, long exp) {
-		if(exp == 1 || exp == 0) return a;
+	private static long[][] pow(long[][] a, long exp) {
+		if(exp == 0) return UNIT_MATRIX;
+		if(exp == 1) return INITIAL_MATRIX;
 		
-		long [][] res = pow(a, exp / 2);
+		long[][] res = pow(a, exp / 2);
 		res = multiply(res, res);
 		
-		if(exp % 2 == 1L) res = multiply(res, initialMatrix);
+		if(exp % 2 == 1) res = multiply(res, INITIAL_MATRIX);
 		
 		return res;
 	}
 	
-	public static void main(String [] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		long n = Long.parseLong(br.readLine());
-		long [][] a = new long [LEN][LEN];
 		
-		/*
-		 *                n
-		 *       | 1   1 |    | F(n+1)  F(n)  |
-		 * A^n = |       |  = |               |
-		 *       | 1   0 |    |  F(n)  F(n-1) |
-		 */
+		 /*               n
+		 *       | 1   1 |    | Fib(n+1)  Fib(n) |
+		 * A^n = |       |  = |                  |
+		 *       | 1   0 |    | Fib(n)  Fib(n-1) |   */
 		
-		for(int i = 0; i < LEN; i++) a[i] = initialMatrix[i];
-		
-		// F_n 을 구하려면  A행렬을 n-1제곱 한 뒤 반환된 행렬 A11 원소를 출력하면 된다.
-		System.out.print(pow(a, n - 1)[0][0]);
+		System.out.print(pow(INITIAL_MATRIX, n - 1)[0][0]);
 	}
 }
