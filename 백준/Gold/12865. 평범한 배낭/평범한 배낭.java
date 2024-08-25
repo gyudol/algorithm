61d988651b9a182d5458cd8c49b7316a7d8ac571
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-08-25 15:22:06 +0900

    [Gold V] Title: 평범한 배낭, Time: 144 ms, Memory: 51388 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String [] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
+		int[][] items = new int[n][];
+		int[][] dp = new int[n + 1][k + 1];
+		
+		for(int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int w = Integer.parseInt(st.nextToken()), v = Integer.parseInt(st.nextToken());
+			
+			items[i] = new int[] {w, v};
+		}
+		
+		for(int i = 1; i <= n; i++) {
+			int itemWeight = items[i - 1][0], itemValue = items[i - 1][1];
+			
+			for(int w = 1; w <= k; w++) {
+				if(itemWeight > w) dp[i][w] = dp[i - 1][w];
+				else dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - itemWeight] + itemValue);
+			}
+		}
+		
+		System.out.print(dp[n][k]);
+	}
+}
