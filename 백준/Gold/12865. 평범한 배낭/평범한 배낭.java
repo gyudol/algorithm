76737e265f1b3f6a138cc0bc23a08d28ed3ed540
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-08-26 23:27:12 +0900

    [Gold V] Title: 평범한 배낭, Time: 144 ms, Memory: 51300 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String [] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken()), k = Integer.parseInt(st.nextToken());
