721c73d51b19f716f0b0c5b3ad3fe11d24ea016e
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-08-26 23:13:38 +0900

    [Silver II] Title: 연속합, Time: 168 ms, Memory: 22604 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static final int MIN = -100000001;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] dp = new int[n + 1];
+		int max = MIN;
+		
+		Arrays.fill(dp, MIN);
+		
+		for(int i = 1; i <= n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			
+			dp[i] = Math.max(num, dp[i - 1] + num);
+			max = Math.max(max, dp[i]);
+		}
+		
+		System.out.print(max);
+	}
+}
