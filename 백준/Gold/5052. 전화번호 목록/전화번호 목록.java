e01a695954bd7c315b5b43cba6f86e6e48f6cf45
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-09-08 18:07:08 +0900

    [Gold IV] Title: 전화번호 목록, Time: 452 ms, Memory: 225688 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+	private static class Node {
+		private Map<Character, Node> children = new HashMap<>();
+		
+		private void add(String phone, int offset) {
+			int length = phone.length() - offset;
+		
+			if(length > 0) {
+				char c = phone.charAt(offset);
+				Node child = children.getOrDefault(c, new Node());
+				
+				child.add(phone, offset + 1);
+				children.put(c, child);
+			}
+		}
+		
+		private boolean isConsistent(String phone, int depth) {
+			char c = phone.charAt(depth);
+			
+			if(depth == phone.length() - 1) {
+				if(children.get(c).children.size() == 0) return true;
+				return false;
+			}
+			
+			return children.get(c).isConsistent(phone, depth + 1);
+		}
+	}
+	
+	private static boolean isConsistent(Node trie, String[] phones, int maxLen) {
+		for(String phone : phones) {
+			if(phone.length() == maxLen) continue;	// 전화번호 같은 경우 없으므로 가장 긴 전화번호들은 건너뜀
+			if(!trie.isConsistent(phone, 0)) return false;
+		}
+		
+		return true;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder result = new StringBuilder();
+		final int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 0; tc < T; tc++) {
+			final int N = Integer.parseInt(br.readLine());
+			Node trie = new Node();
+			String[] phones = new String[N];
+			int maxLen = 0;
+			
+			for(int i = 0; i < N; i++) {
+				phones[i] = br.readLine();
+				trie.add(phones[i], 0);
+				int len = phones[i].length();
+				
+				if(len > maxLen) maxLen = len;
+			}
+			
+			if(isConsistent(trie, phones, maxLen)) result.append('Y').append('E').append('S').append('\n');
+			else result.append('N').append('O').append('\n');
+		}
+		
+		System.out.print(result);
+	}
+}
