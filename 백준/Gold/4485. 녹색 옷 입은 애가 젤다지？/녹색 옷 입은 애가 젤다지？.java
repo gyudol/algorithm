77510189b12ea3a2aa8889c04e87f7a1c90525f6
Gyuchan <83599750+gyudol@users.noreply.github.com>
2024-09-08 15:42:12 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 220 ms, Memory: 23056 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static final int[][] DIR = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+	
+	private static class State implements Comparable<State> {
+		private int row;
+		private int col;
+		private int rupoor;
+		
+		private State(int row, int col, int rupoor) {
+			this.row = row;
+			this.col = col;
+			this.rupoor = rupoor;
+		}
+		
+		@Override
+		public int compareTo(State other) {
+			return Integer.compare(this.rupoor, other.rupoor);
+		}
+	}
+	
+	private static int bfs(int n, int[][] rupoors) {
+		PriorityQueue<State> minHeap = new PriorityQueue<>();
+		boolean[][] isVisited = new boolean[n][n];
+		
+		minHeap.offer(new State(0, 0, rupoors[0][0]));
+		
+		while(!minHeap.isEmpty()) {
+			State state = minHeap.poll();
+			int row = state.row, col = state.col, rupoor = state.rupoor;
+			
+			if(row == n - 1 && col == n - 1) return rupoor;
+			
+			for(int[] d : DIR) {
+				int nextRow = row + d[0], nextCol = col + d[1];
+				
+				if(nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n 
+						|| isVisited[nextRow][nextCol]) continue;
+				isVisited[row][col] = true;
+				
+				minHeap.offer(new State(nextRow, nextCol, rupoor + rupoors[nextRow][nextCol]));
+			}
+		}
+		
+		return -1;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder result = new StringBuilder();
+		int tc = 1;
+		
+		while(true) {
+			int n = Integer.parseInt(br.readLine());
+			
+			if(n == 0) break;
+			
+			int[][] rupoors = new int[n][n];
+			
+			for(int i = 0; i < n; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				
+				for(int j = 0; j < n; j++) rupoors[i][j] = Integer.parseInt(st.nextToken());
+			}
+			
+			result.append('P').append('r').append('o').append('b').append('l').append('e').append('m')
+				.append(' ').append(tc++).append(':').append(' ').append(bfs(n, rupoors)).append('\n');
+		}
+		
+		System.out.print(result);
+	}
+}
