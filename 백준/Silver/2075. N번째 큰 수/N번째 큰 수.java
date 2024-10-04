45b8d4f973daa7ea1591667c3e94e150ff74ac2c
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2024-10-05 02:15:44 +0900

    [Silver II] Title: N번째 큰 수, Time: 732 ms, Memory: 261320 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -4,25 +4,27 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	private static int N;
-	
-	private static int getNthLargest(PriorityQueue<Integer> pq) {
-		for(int i = 0; i < N - 1; i++) pq.poll();
-		
-		return pq.poll();
-	}
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> pq = new PriorityQueue<>((n1, n2) -> Integer.compare(n2, n1));
+		final int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		for(int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) pq.offer(Integer.parseInt(st.nextToken()));
+		
+		for(int i = 1; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
 			
-			for(int j = 0; j < N; j++) pq.offer(Integer.parseInt(st.nextToken()));
+			for(int j = 0; j < N; j++) {
+				int num = Integer.parseInt(st.nextToken());
+				
+				if(pq.peek() < num) {
+					pq.poll();
+					pq.offer(num);
+				}
+			}
 		}
 		
-		System.out.print(getNthLargest(pq));
+		System.out.print(pq.poll());
 	}
 }
