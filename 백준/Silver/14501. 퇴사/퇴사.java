ad3cabf22b44b44f6af50cfb5ece94196aca8532
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2024-10-05 17:05:50 +0900

    [Silver III] Title: 퇴사, Time: 64 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static class Counseling {
+		private int time;
+		private int revenue;
+		
+		private Counseling(int time, int revenue) {
+			this.time = time;
+			this.revenue = revenue;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		final int N = Integer.parseInt(br.readLine());
+		Counseling[] counseling = new Counseling[N];
+		int[] dp = new int[N + 1];
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			counseling[i] = new Counseling(Integer.parseInt(st.nextToken()), 
+					Integer.parseInt(st.nextToken()));
+		}
+		
+		for(int i = 0; i < N; i++) {
+			int nextDay = i + counseling[i].time;
+			
+			if(nextDay > N) continue;
+			
+			dp[nextDay] = Math.max(dp[nextDay], dp[i] + counseling[i].revenue);
+			for(int j = nextDay + 1; j <= N; j++) dp[j] = Math.max(dp[j], dp[nextDay]);
+		}
+		
+		System.out.print(dp[N]);
+	}
+}
