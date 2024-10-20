bfe92bcbd32ec632e6217146ad365e1b8fd01850
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2024-10-20 17:44:42 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 412 ms, Memory: 234708 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	private static char[][] stars;
+	
+	private static void buildStars(int row, int col, int len) {
+		if(len == 3) {
+			stars[row][col] = '*';
+			stars[row + 1][col - 1] = stars[row + 1][col + 1] = '*';
+			stars[row + 2][col - 2] = stars[row + 2][col - 1] = stars[row + 2][col] 
+					= stars[row + 2][col + 1] = stars[row + 2][col + 2] = '*';
+			
+			return;
+		}
+		
+		int half = len / 2;
+		
+		buildStars(row, col, half);
+		buildStars(row + half, col - half, half);
+		buildStars(row + half, col + half, half);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder result = new StringBuilder();
+		final int N = Integer.parseInt(br.readLine());		
+		stars = new char[N][N * 2 - 1];
+		
+		for(char[] row : stars) Arrays.fill(row, ' ');
+		buildStars(0, N - 1, N);	// (0, N - 1) is top
+		
+		for(char[] row : stars) {
+			for(char star : row) result.append(star);
+			result.append('\n');
+		}
+		
+		System.out.print(result);
+	}
+}
