372a4113d57021370ba7fbf6b527e8f0cbd9825e
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2024-10-23 22:02:48 +0900

    [D2] Title: 간단한 소인수분해, Time: 121 ms, Memory: 17,420 KB -BaekjoonHub

---
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	private static final int[] BASE = {2, 3, 5, 7, 11};
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder result = new StringBuilder();
+		final int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			int[] exp = new int[BASE.length];
+			
+			for(int idx = 0; idx < BASE.length; idx++) {
+				while(N % BASE[idx] == 0) {
+					N /= BASE[idx];
+					exp[idx]++;
+				}
+			}
+			
+			result.append('#').append(tc).append(' ');
+			for(int num : exp) result.append(num).append(' ');
+			result.append('\n');
+		}
+		
+		System.out.print(result);
+	}
+}
