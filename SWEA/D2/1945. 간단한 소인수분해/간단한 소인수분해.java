db2e80408071fa5a01b4f4c5c8bf140ff894fc91
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2024-10-23 22:06:24 +0900

    [D2] Title: 간단한 소인수분해, Time: 165 ms, Memory: 17,140 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.java
@@ -1,9 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Solution {
 	private static final int[] BASE = {2, 3, 5, 7, 11};
+	
+	private static int[] findExp(int N) {
+		int[] exp = new int[BASE.length];
+
+		for(int idx = 0; idx < BASE.length; idx++) {
+			while(N % BASE[idx] == 0) {
+				N /= BASE[idx];
+				exp[idx]++;
+			}
+		}
+		
+		return exp;
+	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,15 +23,7 @@ public class Solution {
 		final int T = Integer.parseInt(br.readLine());
 		
 		for(int tc = 1; tc <= T; tc++) {
-			int N = Integer.parseInt(br.readLine());
-			int[] exp = new int[BASE.length];
-			
-			for(int idx = 0; idx < BASE.length; idx++) {
-				while(N % BASE[idx] == 0) {
-					N /= BASE[idx];
-					exp[idx]++;
-				}
-			}
+			int[] exp = findExp(Integer.parseInt(br.readLine()));
 			
 			result.append('#').append(tc).append(' ');
 			for(int num : exp) result.append(num).append(' ');
