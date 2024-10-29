58defd4c3e3f9c002a09ff016b07f9b8b493dce2
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2024-10-29 10:07:35 +0900

    [D2] Title: 두 개의 숫자열, Time: 107 ms, Memory: 18,372 KB -BaekjoonHub

---
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+ 
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder result = new StringBuilder();
+        final int T = Integer.parseInt(br.readLine());
+         
+        for(int tc = 1; tc <= T; tc++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+            final int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+            int[] A = new int[N];	// smaller
+            int[] B = new int[M];	// bigger
+        	int max = Integer.MIN_VALUE;
+        	
+        	st = new StringTokenizer(br.readLine());
+        	for(int i = 0; i < A.length; i++) A[i] = Integer.parseInt(st.nextToken());
+        	st = new StringTokenizer(br.readLine());
+        	for(int i = 0; i < B.length; i++) B[i] = Integer.parseInt(st.nextToken());
+        	
+        	if(N > M) {		// swap
+        		int[] tmp = A;
+        		A = B;
+        		B = tmp;
+        	}
+        	
+        	for(int offset = 0; offset <= B.length - A.length; offset++) {
+        		int sum = 0;
+        		
+        		for(int idx = 0; idx < A.length; idx++) sum += A[idx] * B[idx + offset];
+        		max = Math.max(sum, max);
+        	}
+        	
+        	result.append('#').append(tc).append(' ').append(max).append('\n');
+        }
+         
+        System.out.print(result);
+    }
+}
