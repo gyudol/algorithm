3504bbacd9ba2291cfc7477c832b35afa7aacc82
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2024-10-29 18:54:36 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 39.90 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        return Arrays.stream(strings).sorted((s1, s2) -> {
+            if(s1.charAt(n) == s2.charAt(n)) return s1.compareTo(s2);
+            return Integer.compare(s1.charAt(n), s2.charAt(n));
+        }).toArray(String[]::new);
+    }
+}
