86edde2bd8d81ebae020aaf74f3256d61ea2f2d6
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2024-10-29 11:15:59 +0900

    [D2] Title: 두 개의 숫자열, Time: 99 ms, Memory: 16,884 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.java
@@ -3,6 +3,19 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
  
 public class Solution {
+	private static int getMaxValue(int[] smaller, int[] bigger) {
+		int max = Integer.MIN_VALUE;
+		
+		for(int offset = 0; offset <= bigger.length - smaller.length; offset++) {
+    		int sum = 0;
+    		
+    		for(int idx = 0; idx < smaller.length; idx++) sum += smaller[idx] * bigger[idx + offset];
+    		max = Math.max(sum, max);
+    	}
+		
+		return max;
+	}
+	
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder result = new StringBuilder();
@@ -10,30 +23,16 @@ public class Solution {
          
         for(int tc = 1; tc <= T; tc++) {
         	StringTokenizer st = new StringTokenizer(br.readLine());
-            final int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
-            int[] A = new int[N];	// smaller
-            int[] B = new int[M];	// bigger
-        	int max = Integer.MIN_VALUE;
+        	final int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
+        	final int[] A = new int[N];
+        	final int[] B = new int[M];
         	
         	st = new StringTokenizer(br.readLine());
         	for(int i = 0; i < A.length; i++) A[i] = Integer.parseInt(st.nextToken());
         	st = new StringTokenizer(br.readLine());
         	for(int i = 0; i < B.length; i++) B[i] = Integer.parseInt(st.nextToken());
         	
-        	if(N > M) {	// swap
-        		int[] tmp = A;
-        		A = B;
-        		B = tmp;
-        	}
-        	
-        	for(int offset = 0; offset <= B.length - A.length; offset++) {
-        		int sum = 0;
-        		
-        		for(int idx = 0; idx < A.length; idx++) sum += A[idx] * B[idx + offset];
-        		max = Math.max(sum, max);
-        	}
-        	
-        	result.append('#').append(tc).append(' ').append(max).append('\n');
+        	result.append('#').append(tc).append(' ').append(N <= M ? getMaxValue(A, B) : getMaxValue(B, A)).append('\n');
         }
          
         System.out.print(result);
