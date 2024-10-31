02f40ff5b9b736c2c5fe448c4db9a4018d9358e6
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2024-10-31 20:48:11 +0900

    [level 1] Title: 시저 암호, Time: 0.92 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,7 +1,7 @@
 class Solution {
     private static final int LENGTH = 26;
     
-    private char scisorEncryption(char c, int n) {
+    private char caesarEncryption(char c, int n) {
         if(c == ' ') return c;
         
         int offset = c >= 'a' && c <= 'z' ? 'a' : 'A';
@@ -13,7 +13,7 @@ class Solution {
     public String solution(String s, int n) {
         StringBuilder result = new StringBuilder();
         
-        for(char c : s.toCharArray()) result.append(scisorEncryption(c, n));
+        for(char c : s.toCharArray()) result.append(caesarEncryption(c, n));
         return result.toString();
     }
 }
