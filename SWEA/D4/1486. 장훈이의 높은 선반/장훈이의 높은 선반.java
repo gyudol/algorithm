5c14c415fab69a7a5d4c44d6032933f9d61e7d9d
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-01-13 23:11:27 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 118 ms, Memory: 19,100 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+	private static int dfs(int sum, int src, int N, int B, int S, int[] heights) {
+		if(sum >= B) return sum;
+		
+		int min = Integer.MAX_VALUE;
+		
+		for(int i = src; i < N; i++) min = Math.min(min, dfs(sum + heights[i], i + 1, N, B, S, heights));
+		
+		return min;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 StringBuilder result = new StringBuilder();
+		 final int T = Integer.parseInt(br.readLine());
+		 
+		 for(int tc = 1; tc <= T; tc++) {
+			 StringTokenizer st = new StringTokenizer(br.readLine());
+			 final int N = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken());
+			 int S = 0;
+			 int[] heights = new int[N];
+			 
+			 st = new StringTokenizer(br.readLine());
+			 for(int i = 0; i < N; i++) {
+				 heights[i] = Integer.parseInt(st.nextToken());
+				 S += heights[i];
+			 }
+			 
+			 Arrays.sort(heights);
+			 result.append('#').append(tc).append(' ').append(dfs(0, 0, N, B, S, heights) - B).append('\n');
+		 }
+		 
+		 System.out.print(result);
+	}
+}
