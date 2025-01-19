a00bafe1419be05bb9237f7ddad4d6029bd862e2
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-01-19 22:41:31 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 188 ms, Memory: 37,352 KB -BaekjoonHub

--- SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -1,96 +1,59 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 
 public class Solution {
 	private static final int[][] DIR = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, 
 			{1, -1}, {0, -1}, {-1, -1}};
 	
-	private static class State implements Comparable<State> {
-		private int row;
-		private int col;
-		private int value;
-		
-		private State(int row, int col, int value) {
-			this.row = row;
-			this.col = col;
-			this.value = value;
-		}
-		
-		@Override
-		public int compareTo(State other) {
-			return Integer.compare(value, other.value);
-		}
-	}
-	
-	private static int mineCnt(int row, int col, int N, char[][] board) {
-		int cnt = 0;
+	private static int countMines(int row, int col, int N, char[][] board) {
+		int mines = 0;
 		
 		for(int[] d : DIR) {
 			int nextRow = row + d[0], nextCol = col + d[1];
 			
-			if(nextRow < 0 || nextRow >= N || nextCol < 0 || nextCol >= N
-					|| board[nextRow][nextCol] == '.') continue;
-			
-			cnt++;
+			if(nextRow >= 0 && nextRow < N && nextCol >= 0 && nextCol < N
+					&& board[nextRow][nextCol] == '*') mines++;
 		}
 		
-		return cnt;
+		return mines;
 	}
 	
-	private static void dfs(int row, int col, int N, boolean[][] isVisited, char[][] board) {
+	private static void dfs(int row, int col, int N, char[][] board, boolean[][] isVisited) {
 		if(isVisited[row][col]) return;
 		isVisited[row][col] = true;
 		
-		if(mineCnt(row, col, N, board) > 0) return;
+		if(countMines(row, col, N, board) > 0) return;
 		
 		for(int[] d : DIR) {
 			int nextRow = row + d[0], nextCol = col + d[1];
 			
-			if(nextRow < 0 || nextRow >= N || nextCol < 0 || nextCol >= N
-					|| board[nextRow][nextCol] == '*') continue;
-			
-			dfs(nextRow, nextCol, N, isVisited, board);
+			if(nextRow >= 0 && nextRow < N && nextCol >= 0 && nextCol < N
+					&& board[nextRow][nextCol] == '.') dfs(nextRow, nextCol, N, board, isVisited);
 		}
 	}
 	
-	private static int bfs(int N, char[][] board) {
-		PriorityQueue<State> pq = new PriorityQueue<>();
+	private static int mineSweeper(int N, char[][] board) {
+		int clickCnt = 0;
 		boolean[][] isVisited = new boolean[N][N];
-		int cnt = 0;
 		
 		for(int row = 0; row < N; row++) {
 			for(int col = 0; col < N; col++) {
-				if(board[row][col] == '*') {
-					isVisited[row][col] = true;
-					continue;
+				if(board[row][col] == '*') isVisited[row][col] = true;
+				
+				if(!isVisited[row][col] && countMines(row, col, N, board) == 0) {
+					dfs(row, col, N, board, isVisited);
+					clickCnt++;
 				}
-				
-				int mineCnt = 0;
-				
-				for(int[] d : DIR) {
-					int nextRow = row + d[0], nextCol = col + d[1];
-					
-					if(nextRow < 0 || nextRow >= N || nextCol < 0 || nextCol >= N
-							|| board[nextRow][nextCol] == '.') continue;
-					mineCnt++;
-				}
-				
-				pq.offer(new State(row, col, mineCnt));
 			}
 		}
 		
-		while(!pq.isEmpty()) {
-			State state = pq.poll();
-			int row = state.row, col = state.col;
-			
-			if(isVisited[row][col]) continue;
-			
-			dfs(row, col, N, isVisited, board);
-			cnt++;
+		for(int row = 0; row < N; row++) {
+			for(int col = 0; col < N; col++) {
+				if(!isVisited[row][col]) clickCnt++;
+			}
 		}
 		
-		return cnt;
+		return clickCnt;
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -104,7 +67,7 @@ public class Solution {
 			
 			for(int row = 0; row < N; row++) board[row] = br.readLine().toCharArray();
 			
-			result.append('#').append(tc).append(' ').append(bfs(N, board)).append('\n');
+			result.append('#').append(tc).append(' ').append(mineSweeper(N, board)).append('\n');
 		}
 		
 		System.out.print(result);
