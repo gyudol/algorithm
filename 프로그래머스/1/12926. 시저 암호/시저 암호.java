a77fcfb4be50fed47990aea6b39cad0d846d01d0
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-02-03 16:36:09 +0900

    [level 1] Title: 시저 암호, Time: 5.20 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,19 +1,23 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Solution {
     private static final int LENGTH = 26;
     
-    private char caesarEncryption(char c, int n) {
+    private char caesarCipher(char c, int n) {
         if(c == ' ') return c;
         
-        int offset = c >= 'a' && c <= 'z' ? 'a' : 'A';
-        int pos = c - offset;
+        char offset = c >= 'A' && c <= 'Z' ? 'A' : 'a';
         
-        return (char) (((pos + n) % LENGTH) + offset);
+        return (char) ((c - offset + n) % LENGTH + offset);
     }
     
     public String solution(String s, int n) {
-        StringBuilder result = new StringBuilder();
-        
-        for(char c : s.toCharArray()) result.append(caesarEncryption(c, n));
-        return result.toString();
+        return IntStream.range(0, s.length())
+            .map(i -> caesarCipher(s.charAt(i), n))
+            .collect(StringBuilder::new,
+                    StringBuilder::appendCodePoint,
+                    StringBuilder::append)
+            .toString();
     }
 }
