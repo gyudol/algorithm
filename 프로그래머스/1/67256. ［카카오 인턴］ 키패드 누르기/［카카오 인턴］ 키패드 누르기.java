5fdf9741a2097b66c54e7ad52c18e3922ed98bb6
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-02-08 00:13:23 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.43 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,61 +1,43 @@
 class Solution {
-    private static final int[][] KEYPAD = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {-1, 0, -1}};
+    private static final int[][] KEYPAD = {{3, 1}, {0, 0}, {0, 1}, {0, 2}, {1, 0}, 
+                                           {1, 1}, {1, 2}, {2, 0}, {2, 1}, {2, 2}};
     private static StringBuilder result = new StringBuilder();
-    private static int leftRow = 3, leftCol = 0, rightRow = 3, rightCol = 2;
+    private static Hand left, right;
     
-    private static int dist(int number, boolean isLeft) {   // only middle number
-        for(int row = 0; row < KEYPAD.length; row++) {
-            if(number == KEYPAD[row][1]) {
-                if(isLeft) return Math.abs(leftRow - row) + Math.abs(leftCol - 1);
-                else return Math.abs(rightRow - row) + Math.abs(rightCol - 1);
-            }
-        }
+    private class Hand {
+        int row;
+        int col;
+        boolean isLeft;
         
-        return -1;
-    }
-    
-    private static void move(int number, boolean isLeft) {
-        for(int row = 0; row < KEYPAD.length; row++) {
-            for(int col = 0; col < KEYPAD[row].length; col++) {
-                if(number == KEYPAD[row][col]) {
-                    if(isLeft) {
-                        leftRow = row;  leftCol = col;
-                    }
-                    else {
-                        rightRow = row; rightCol = col;
-                    }
-                    return;
-                }
-            }
+        public Hand(int row, int col, boolean isLeft) {
+            this.row = row;
+            this.col = col;
+            this.isLeft = isLeft;
         }
     }
     
-    private static void move(int number, String hand) {
-        if(number == KEYPAD[0][0] || number == KEYPAD[1][0] || number == KEYPAD[2][0]) {
-            result.append('L');
-            move(number, true);
-        }
-        else if(number == KEYPAD[0][2] || number == KEYPAD[1][2] || number == KEYPAD[2][2]) {
-            result.append('R');
-            move(number, false);
+    private int distance(int num, Hand hand) {
+        return Math.abs(hand.row - KEYPAD[num][0]) + Math.abs(hand.col - KEYPAD[num][1]);
+    }
+    
+    private void move(int num, String hand, Hand left, Hand right) {
+        int leftDist = distance(num, left), rightDist = distance(num, right);
+        
+        if(num % 3 == 1 || ((num % 3 == 2 || num == 0) && 
+                            (leftDist < rightDist || (leftDist == rightDist && hand.equals("left"))))) {
+            result.append('L'); 
+            left.row = KEYPAD[num][0];  left.col = KEYPAD[num][1];
         }
         else {
-            int leftDist = dist(number, true), rightDist = dist(number, false);
-            boolean isLeft = (leftDist < rightDist) || ((leftDist == rightDist) && hand.equals("left"));
-            
-            if(isLeft) {
-                result.append('L');
-                move(number, true);
-            }
-            else {
-                result.append('R');
-                move(number, false);
-            }
+            result.append('R'); 
+            right.row = KEYPAD[num][0];  right.col = KEYPAD[num][1];
         }
     }
     
     public String solution(int[] numbers, String hand) {
-        for(int number : numbers) move(number, hand);
+        Hand left = new Hand(3, 0, true), right = new Hand(3, 2, false);
+        
+        for(int number : numbers) move(number, hand, left, right);
         
         return result.toString();
     }
