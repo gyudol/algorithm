1c57d88ea785b9f0caa62355534674ead09bc5b4
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-02-08 00:21:24 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.04 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,8 +1,8 @@
 class Solution {
     private static final int[][] KEYPAD = {{3, 1}, {0, 0}, {0, 1}, {0, 2}, {1, 0}, 
                                            {1, 1}, {1, 2}, {2, 0}, {2, 1}, {2, 2}};
-    private static StringBuilder result = new StringBuilder();
-    private static Hand left, right;
+    private StringBuilder result = new StringBuilder();
+    private Hand left, right;
     
     private class Hand {
         int row;
@@ -20,11 +20,11 @@ class Solution {
         return Math.abs(hand.row - KEYPAD[num][0]) + Math.abs(hand.col - KEYPAD[num][1]);
     }
     
-    private void move(int num, String hand, Hand left, Hand right) {
+    private void move(int num, boolean isLeftHanded, Hand left, Hand right) {
         int leftDist = distance(num, left), rightDist = distance(num, right);
         
         if(num % 3 == 1 || ((num % 3 == 2 || num == 0) && 
-                            (leftDist < rightDist || (leftDist == rightDist && hand.equals("left"))))) {
+                            (leftDist < rightDist || (leftDist == rightDist && isLeftHanded)))) {
             result.append('L'); 
             left.row = KEYPAD[num][0];  left.col = KEYPAD[num][1];
         }
@@ -37,7 +37,7 @@ class Solution {
     public String solution(int[] numbers, String hand) {
         Hand left = new Hand(3, 0, true), right = new Hand(3, 2, false);
         
-        for(int number : numbers) move(number, hand, left, right);
+        for(int number : numbers) move(number, hand.equals("left"), left, right);
         
         return result.toString();
     }
