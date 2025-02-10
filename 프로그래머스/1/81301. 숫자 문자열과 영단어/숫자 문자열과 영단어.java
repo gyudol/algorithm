6b16b47a0033de937bda97924182ef35ff1a8eba
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-02-10 17:49:13 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.48 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,10 @@
+class Solution {
+    private static String[] words = {"zero", "one", "two", "three", "four", 
+                                     "five", "six", "seven", "eight", "nine"};
+    
+    public int solution(String s) {
+        for(int i = 0; i < words.length; i++) s = s.replaceAll(words[i], Integer.toString(i));
+        
+        return Integer.parseInt(s);
+    }
+}
