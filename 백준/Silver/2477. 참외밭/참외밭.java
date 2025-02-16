83ce8d2abd6a846e863ec931ac1feddc3fe73d63
Gyuchan Kim <sb3g@naver.com>
2025-02-17 00:20:37 +0900

    [Silver II] Title: 참외밭, Time: 64 ms, Memory: 11536 KB

--- 백준/Silver/2477. 참외밭/참외밭.java
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -5,34 +5,34 @@ import java.util.StringTokenizer;
 public class Main {
 	private static final int M = 6;
 	
-	private static int getArea(int[][] moves, int maxHeight, int maxWidth) {
-		int rowDiff = -1, colDiff = -1;
+	private static int getArea(int[] moves, int maxHeight, int maxWidth, int hIdx, int wIdx) {
+		int heightDiff = Math.abs(moves[(hIdx - 1 + M) % M] - moves[(hIdx + 1) % M]),
+				widthDiff = Math.abs(moves[(wIdx - 1 + M) % M] - moves[(wIdx + 1) % M]);
 		
-		for(int i = 0; i < moves.length; i++) {
-			if((moves[i][0] == 1 || moves[i][0] == 2) && maxWidth == moves[i][1]) 
-				rowDiff = Math.abs(moves[(i - 1 + M) % M][1] - moves[(i + 1) % M][1]);
-			if((moves[i][0] == 3 || moves[i][0] == 4) && maxHeight == moves[i][1]) 
-				colDiff = Math.abs(moves[(i - 1 + M) % M][1] - moves[(i + 1) % M][1]);
-		}
-			
-		return maxHeight * maxWidth - rowDiff * colDiff;
+		return maxHeight * maxWidth - heightDiff * widthDiff;
 	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		final int K = Integer.parseInt(br.readLine());
-		int[][] moves = new int[M][2];
-		int maxHeight = 0, maxWidth = 0;
+		int[] moves = new int[M];
+		int maxHeight = 0, maxWidth = 0, hIdx = -1, wIdx = -1;
 		
 		for(int i = 0; i < M; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int d = Integer.parseInt(st.nextToken()), offset = Integer.parseInt(st.nextToken());
-			
-			moves[i][0] = d;	moves[i][1] = offset;
-			if(d == 1 || d == 2) maxWidth = Math.max(offset, maxWidth);
-			else maxHeight = Math.max(offset, maxHeight);
+			moves[i] = offset;
+
+			if((d == 3 || d == 4) && offset > maxHeight) {
+					maxHeight = offset;
+					hIdx = i;
+			}
+			else if((d == 1 || d == 2) && offset > maxWidth) {
+					maxWidth = offset;
+					wIdx = i;
+			}
 		}
 		
-		System.out.print(getArea(moves, maxHeight, maxWidth) * K);
+		System.out.print(getArea(moves, maxHeight, maxWidth, hIdx, wIdx) * K);
 	}
 }
