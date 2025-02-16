eb9b897902a70b87f12df94ebf92a23dabcf8de1
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-02-16 15:07:37 +0900

    [level 2] Title: 피보나치 수, Time: 2.60 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,10 +1,12 @@
 class Solution {
+    private static final int MOD = 1234567;
+    
     public int solution(int n) {
         int[] dp = new int[n + 1];
         
         for(int i = 1; i <= n; i++) {
             if(i <= 1) dp[i] = i;
-            else dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+            else dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
         }
         
         return dp[n];
