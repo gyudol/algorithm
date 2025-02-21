f7bd0314a3bced574529f0232a62381d3b36e3be
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-02-21 15:59:53 +0900

    [D3] Title: 햄버거 다이어트, Time: 117 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -3,6 +3,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 class Solution {
+	static int N, L;
+	static int[][] ingredients;
+	
+	static int hamburgerDiet(int src, int totalScore, int totalCalorie) {
+		if(totalCalorie > L) return 0;
+		
+		int max = totalScore;
+		
+		for(int i = src; i < N; i++) {
+			max = Math.max(hamburgerDiet(i + 1, totalScore + ingredients[i][0], 
+					totalCalorie + ingredients[i][1]), max);
+		}
+		
+		return max;
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder result = new StringBuilder();
@@ -10,26 +26,16 @@ class Solution {
 		
 		for(int tc = 1; tc <= T; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			final int N = Integer.parseInt(st.nextToken()), L = Integer.parseInt(st.nextToken());
-			final int[][] items = new int[N][2];
-			int[][] dp = new int[N + 1][L + 1];
+			N = Integer.parseInt(st.nextToken());	L = Integer.parseInt(st.nextToken());
+			ingredients = new int[N][2];
 			
 			for(int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
-				items[i][0] = Integer.parseInt(st.nextToken());
-				items[i][1] = Integer.parseInt(st.nextToken());
+				ingredients[i][0] = Integer.parseInt(st.nextToken());
+				ingredients[i][1] = Integer.parseInt(st.nextToken());
 			}	
 			
-			for(int i = 1; i <= N; i++) {
-				int itemWeight = items[i - 1][1], itemScore = items[i - 1][0];
-				
-				for(int w = 1; w <= L; w++) {
-					if(itemWeight > w) dp[i][w] = dp[i - 1][w];
-					else dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - itemWeight] + itemScore);
-				}
-			}
-			
-			result.append('#').append(tc).append(' ').append(dp[N][L]).append('\n');
+			result.append('#').append(tc).append(' ').append(hamburgerDiet(0, 0, 0)).append('\n');
 		}
 
 		System.out.print(result);
