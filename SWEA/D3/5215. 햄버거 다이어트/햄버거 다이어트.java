6d359f74198b605d60c392c5eabc4da1eb7b324d
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-02-21 10:28:13 +0900

    [D3] Title: 햄버거 다이어트, Time: 240 ms, Memory: 26,368 KB -BaekjoonHub

---
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Solution {
+	static int N, L;
+	static int[][] ingredients;
+	
+	static int hamburgerDiet(int src, int totalScore, int totalCalorie, boolean[] isSelected) {
+		if(totalCalorie > L) return 0;
+		
+		int max = totalScore;
+		
+		for(int i = src; i < N; i++) {
+			if(isSelected[i]) continue;
+			
+			isSelected[i] = true;
+			max = Math.max(hamburgerDiet(i + 1, totalScore + ingredients[i][0], 
+					totalCalorie + ingredients[i][1], isSelected), max);
+			isSelected[i] = false;
+		}
+		
+		return max;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder result = new StringBuilder();
+		final int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());	L = Integer.parseInt(st.nextToken());
+			ingredients = new int[N][2];
+			
+			for(int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				ingredients[i][0] = Integer.parseInt(st.nextToken());
+				ingredients[i][1] = Integer.parseInt(st.nextToken());
+			}	
+			
+			result.append('#').append(tc).append(' ').append(hamburgerDiet(0, 0, 0, new boolean[N])).append('\n');
+		}
+
+		System.out.print(result);
+	}
+}
