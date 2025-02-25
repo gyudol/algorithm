23d8ddaba144e891cb657b344e9340f0fa21906e
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-02-25 17:39:18 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 232 ms, Memory: 28,776 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -5,34 +5,31 @@ import java.util.StringTokenizer;
 public class Solution {
 	static int N;
 	static int min;
-	static int[][] ingredients;
+	static int[][] synergy;
 	
-	static int getSynergy(boolean[] isSelected, boolean flag) {
-		int sum = 0;
+	static int getSynergyDiff(boolean[] isFoodA) {
+		int sumA = 0, sumB = 0;
 		
 		for(int i = 0; i < N; i++) {
-			if(isSelected[i] == flag) continue;
-			
-			for(int j = 0; j < N; j++) {
-				if(isSelected[j] == flag) continue;
-				
-				sum += ingredients[i][j];
+			for(int j = i + 1; j < N; j++) {
+				if(isFoodA[i] && isFoodA[j]) sumA += synergy[i][j] + synergy[j][i];
+				else if(!isFoodA[i] && !isFoodA[j]) sumB += synergy[i][j] + synergy[j][i];
 			}
 		}
 		
-		return sum;
+		return Math.abs(sumA - sumB);
 	}
 	
-	static void getMinDifference(int src, int depth, boolean[] isSelected) {
+	static void dfs(int src, int depth, boolean[] isFoodA) {
 		if(depth == N / 2) {
-			min = Math.min(min, Math.abs(getSynergy(isSelected, true) - getSynergy(isSelected, false)));
+			min = Math.min(min, getSynergyDiff(isFoodA));
 			return;
 		}
 		
 		for(int i = src; i < N; i++) {
-			isSelected[i] = true;
-			getMinDifference(i + 1, depth + 1, isSelected);
-			isSelected[i] = false;
+			isFoodA[i] = true;
+			dfs(i + 1, depth + 1, isFoodA);
+			isFoodA[i] = false;
 		}
 	}
 	
@@ -44,15 +41,15 @@ public class Solution {
 		for(int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine());
 			min = Integer.MAX_VALUE;
-			ingredients = new int[N][N];
+			synergy = new int[N][N];
 			
 			for(int row = 0; row < N; row++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				
-				for(int col = 0; col < N; col++) ingredients[row][col] = Integer.parseInt(st.nextToken());
+				for(int col = 0; col < N; col++) synergy[row][col] = Integer.parseInt(st.nextToken());
 			}
 			
-			getMinDifference(0, 0, new boolean[N]);
+			dfs(0, 0, new boolean[N]);
 			result.append('#').append(tc).append(' ').append(min).append('\n');
 		}
 	
