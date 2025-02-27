25ebf9c4d6ff0582cdfd15f5efd10f88d74d7769
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-02-27 11:53:37 +0900

    [D4] Title: 의석이의 우뚝 선 산, Time: 203 ms, Memory: 29,056 KB -BaekjoonHub

--- SWEA/D4/4796. 의석이의 우뚝 선 산/의석이의 우뚝 선 산.java
+++ SWEA/D4/4796. 의석이의 우뚝 선 산/의석이의 우뚝 선 산.java
@@ -1,41 +1,42 @@
-import java.io.InputStreamReader;
-import java.io.StreamTokenizer;
-
 class Solution {
-	static int getIntervalCount(int N, int[] heights) {
-		int cnt = 0, src = 0, mid = 0, des = 1;
-		
-		while(des < N) {
-			while(des < N && (heights[des - 1] < heights[des])) mid = des++;
-			while(des < N && (heights[des - 1] > heights[des])) des++;
-
-			cnt += (mid - src) * (des - mid - 1);
-			src = mid = des - 1;
-		}
-		
-		return cnt;
-	}
-	
-	static int readNextInt(StreamTokenizer st) throws Exception {
-		st.nextToken();
-		return (int) st.nval;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		StreamTokenizer st = new StreamTokenizer(new InputStreamReader(System.in));
-		StringBuilder result = new StringBuilder();
-		final int T = readNextInt(st);
-		
-		for(int tc = 1; tc <= T; tc++) {
-			final int N = readNextInt(st);
-			int[] heights = new int[N];
-			
-			for(int i = 0; i < N; i++) heights[i] = readNextInt(st);
-			
-			result.append('#').append(tc).append(' ')
-				.append(getIntervalCount(N, heights)).append('\n');
-		}
-		
-		System.out.print(result);
-	}
+    static int getIntervalCount(int N, int[] heights) {
+        int cnt = 0, src = 0, mid = 0, des = 1;
+         
+        while(des < N - 1) {
+            while(des < N && (heights[des - 1] < heights[des])) mid = des++;
+            while(des < N && (heights[des - 1] > heights[des])) des++;
+ 
+            cnt += (mid - src) * (des - mid - 1);
+            src = mid = des - 1;
+        }
+         
+        return cnt;
+    }
+    
+    static int readInt() throws Exception {
+    	int c, n = System.in.read() & 15;
+    	
+    	while((c = System.in.read()) >= 48) 
+    		n = (n << 3) + (n << 1) + (c & 15);
+    	if(c == 13) System.in.read();
+    	
+    	return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        StringBuilder result = new StringBuilder();
+        final int T = readInt();
+         
+        for(int tc = 1; tc <= T; tc++) {
+            final int N = readInt();
+            int[] heights = new int[N];
+            
+            for(int i = 0; i < N; i++) heights[i] = readInt();
+             
+            result.append('#').append(tc).append(' ')
+                .append(getIntervalCount(N, heights)).append('\n');
+        }
+         
+        System.out.print(result);
+    }
 }
