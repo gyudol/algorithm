00d7b5d94d606eff5ae0939ed89a23c3cf54678d
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-03-14 23:39:21 +0900

    [Bronze II] Title: 방 배정, Time: 72 ms, Memory: 11464 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -0,0 +1,33 @@
+class Main {
+	static final int GRADE = 6;
+	static final int GENDER = 2;
+	
+	static int readInt() throws Exception {
+		int c, n = 0;
+		
+		while ((c = System.in.read()) <= 32);
+		do {
+			n = (n << 3) + (n << 1) + (c & 15);
+		} while ((c = System.in.read()) >= 48);
+		
+		return n;
+	}
+	
+	public static void main(String [] args) throws Exception {
+		final int N = readInt(), K = readInt();
+		int[][] students = new int[GENDER][GRADE + 1];
+		int roomCnt = 0;
+		
+		for (int i = 0; i < N; i++)	students[readInt()][readInt()]++;
+		
+		for (int grade = 1; grade <= GRADE; grade++) {
+			for (int gender = 0; gender < GENDER; gender++) {
+				roomCnt += students[gender][grade] / K;
+				
+				if (students[gender][grade] % K > 0) roomCnt++;
+			}
+		}
+		
+		System.out.print(roomCnt);
+	}
+}
