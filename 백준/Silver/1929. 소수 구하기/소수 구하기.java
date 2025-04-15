68ff847713bc655da8181969ebf29955d021800c
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-04-15 12:30:46 +0900

    [Silver III] Title: 소수 구하기, Time: 172 ms, Memory: 17832 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -2,23 +2,24 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-	public static void main(String [] args) throws Exception {
+class Main {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder result = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int m = Integer.parseInt(st.nextToken()), n = Integer.parseInt(st.nextToken());
-		boolean [] isNotPrime = new boolean [n + 1];
+
+		final int M = Integer.parseInt(st.nextToken()), N = Integer.parseInt(st.nextToken());
+		boolean[] isNotPrime = new boolean[N + 1];
 		isNotPrime[0] = isNotPrime[1] = true;
 		
-		for(int i = 2; i <= Math.sqrt(n); i++) {
-			if(isNotPrime[i]) continue;
+		for (int i = 2; i <= Math.sqrt(N); i++) {
+			if (isNotPrime[i]) continue;
 			
-			for(int j = i * i; j <= n; j += i) isNotPrime[j] = true;
+			for (int j = i * i; j <= N; j += i) isNotPrime[j] = true;
 		}
 		
-		for(int i = m; i <= n; i++) {
-			if(!isNotPrime[i]) result.append(i).append('\n');
+		for (int num = M; num <= N; num++) {
+			if (!isNotPrime[num]) result.append(num).append('\n');
 		}
 		
 		System.out.print(result);
