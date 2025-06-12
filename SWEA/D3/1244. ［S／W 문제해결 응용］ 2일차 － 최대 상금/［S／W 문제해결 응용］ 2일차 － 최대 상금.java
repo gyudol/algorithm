f1cdc6cf33af9af6bce61a905828f3424b4935ee
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-06-13 01:07:53 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 100 ms, Memory: 31,052 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -1,7 +1,11 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
 	static int M;
 	static int max;
 	static char[] digits;
+	static Set<State> visited;
 	
 	static void swap(int i, int j) {
 		char temp = digits[i];
@@ -14,6 +18,10 @@ class Solution {
 			max = Math.max(Integer.parseInt(new String(digits)), max);
 			return;
 		}
+		
+		State current = new State(new String(digits), depth);
+		if (visited.contains(current)) return;
+		visited.add(current);
 		
 		for (int i = 0; i < digits.length; i++) {
 			for (int j = i + 1; j < digits.length; j++) {
@@ -46,6 +54,7 @@ class Solution {
 			M = readInt();
 			max = Integer.MIN_VALUE;
 			digits = Integer.toString(N).toCharArray();
+			visited = new HashSet<>();
 			
 			// 자릿수 이상의 교환은 중복된 조합만 반복하므로 제한
 			M = Math.min(digits.length, M);
@@ -55,5 +64,28 @@ class Solution {
 		}
 		
 		System.out.print(result);
+	}
+	
+	static class State {
+		String number;
+		int depth;
+		
+		State(String number, int depth) {
+			this.number = number;
+			this.depth = depth;
+		}
+		
+		@Override
+		public int hashCode() {
+			return M * number.hashCode() + depth;
+		}
+		
+		@Override
+		public boolean equals(Object o) {
+			if (!(o instanceof State)) return false;
+			State state = (State) o;
+			
+			return number.equals(state.number) && depth == state.depth;
+		}
 	}
 }
