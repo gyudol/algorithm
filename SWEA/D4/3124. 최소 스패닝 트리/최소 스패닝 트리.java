1795105645238ff7cc4cb8bb5829e7a016d33b04
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-06-17 15:10:43 +0900

    [D4] Title: 최소 스패닝 트리, Time: 1,487 ms, Memory: 97,708 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,93 +1,93 @@
 import java.util.ArrayList;
-import java.util.PriorityQueue;
-
+import java.util.Collections;
+import java.util.List;
+ 
 class Solution {
-	static int V, E;
-	static ArrayList<ArrayList<Node>> edges;
-	
-	static long prim() {
-		PriorityQueue<Node> heap = new PriorityQueue<>();
-		boolean[] isVisited = new boolean[V + 1];
-		long costSum = 0;
-		
-		isVisited[1] = true;
-		for (Node src : edges.get(1)) {
-			heap.offer(new Node(src.v,  src.dist));
-		}
-		
-		while (!heap.isEmpty()) {
-			Node node = heap.poll();
-			int u = node.v, dist = node.dist;
-			
-			if (isVisited[u]) continue;
-			
-			isVisited[u] = true;
-			costSum += dist;
-			
-			for (Node des : edges.get(u)) {
-				int v = des.v;
-				
-				if (isVisited[v]) continue;
-				
-				heap.offer(new Node(v, des.dist));
-			}
-		}
-		
-		return costSum;
-	}
-	
-	static int readInt() throws Exception {
-		int c, n = 0;
-		
-		while ((c = System.in.read()) <= 32);
-		boolean isNegative = c == 45;
-		
-		if (isNegative) c = System.in.read();
-		do {
-			n = (n << 3) + (n << 1) + (c & 15);
-		} while ((c = System.in.read()) >= 48);
-		
-		return isNegative ? ~n + 1 : n;
-	}
-	
-	public static void main(String[] args) throws Exception {
-		final int T = readInt();
-		StringBuilder result = new StringBuilder();
-		
-		for (int tc = 1; tc <= T; tc++) {
-			V = readInt();
-			E = readInt();
-			edges = new ArrayList<>();
-			
-			for (int i = 0; i <= V; i++) {
-				edges.add(new ArrayList<>());
-			}
-			
-			for (int i = 0; i < E; i++) {
-				int u = readInt(), v = readInt(), dist = readInt();
-				
-				edges.get(u).add(new Node(v, dist));
-				edges.get(v).add(new Node(u, dist));
-			}
-			
-			result.append('#').append(tc).append(' ')
-				.append(prim()).append('\n');
-		}
-		
-		System.out.print(result);
-	}
-	
-	static class Node implements Comparable<Node> {
-		int v, dist;
-		
-		Node(int v, int dist) {
-			this.v = v;
-			this.dist = dist;
-		}
-		
-		@Override
-		public int compareTo(Node other) {
-			return Integer.compare(dist, other.dist);
-		}
-	}
+    static int V, E;
+    static int[] parents;
+    static List<Edge> edges;
+     
+    static int root(int node) {
+        if (parents[node] == node) return node;
+        return parents[node] = root(parents[node]);
+    }
+     
+    static boolean merge(int u, int v) {
+        int rootU = root(u), rootV = root(v);
+         
+        if (rootU == rootV) return false;
+        parents[rootV] = rootU;
+         
+        return true;
+    }
+     
+    static long kruskal() {
+        long totalDist = 0;
+        int vCnt = 1;
+         
+        Collections.sort(edges);
+         
+        for (Edge edge : edges) {
+            if (!merge(edge.u, edge.v)) continue;
+             
+            totalDist += edge.dist;
+            if (++vCnt == V) break;
+        }
+         
+        return totalDist;
+    }
+     
+    static int readInt() throws Exception {
+        int c, n = 0;
+         
+        while ((c = System.in.read()) <= 32);
+        boolean isNegative = c == 45;
+         
+        if (isNegative) c = System.in.read();
+        do {
+            n = (n << 3) + (n << 1) + (c & 15);
+        } while ((c = System.in.read()) >= 48);
+         
+        return isNegative ? ~n + 1 : n;
+    }
+     
+    public static void main(String[] args) throws Exception {
+        final int T = readInt();
+        StringBuilder result = new StringBuilder();
+         
+        for (int tc = 1; tc <= T; tc++) {
+            V = readInt();
+            E = readInt();
+            parents = new int[V + 1];
+            edges = new ArrayList<>();
+             
+            for (int node = 1; node <= V; node++) {
+                parents[node] = node;
+            }
+             
+            for (int i = 0; i < E; i++) {
+                edges.add(new Edge(readInt(), readInt(), readInt()));
+            }
+             
+            result.append('#').append(tc).append(' ')
+                .append(kruskal()).append('\n');
+        }
+         
+        System.out.print(result);
+    }
+     
+    static class Edge implements Comparable<Edge> {
+        int u, v, dist;
+         
+        Edge(int u, int v, int dist) {
+            this.u = u;
+            this.v = v;
+            this.dist = dist;
+        }
+         
+        @Override
+        public int compareTo(Edge other) {
+            return Integer.compare(dist, other.dist);
+        }
+    }
 }
