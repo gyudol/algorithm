57d66c270562f80b77fb2b74c797e0591a43a349
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-06-19 21:19:19 +0900

    [level 2] Title: 카펫, Time: 4.75 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,8 +1,6 @@
 class Solution {
-    static final int MAX_WIDTH = 2500;
-    
     public int[] solution(int brown, int yellow) {
-        for (int width = 3; width <= MAX_WIDTH; width++) {
+        for (int width = 3; width < brown / 2; width++) {
             for (int height = 3; height <= width; height++) {
                 if (width * height == brown + yellow && 
                     (width - 2) * (height - 2) == yellow) {  // xy = brown + yellow
