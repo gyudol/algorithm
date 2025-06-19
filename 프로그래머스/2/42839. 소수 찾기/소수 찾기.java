d1add4be6c9685d1ff3d9d831e283e819f65434d
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-06-19 19:16:26 +0900

    [level 2] Title: 소수 찾기, Time: 68.86 ms, Memory: 99.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,46 @@
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    static final int MAX_NUMBER = 9_999_999;
+    
+    static void generate(int depth, int piece, String numbers, boolean[] isVisited, Set<Integer> digitSet) {
+        digitSet.add(piece);
+        if (depth == numbers.length()) return;
+        
+        for (int i = 0; i < numbers.length(); i++) {
+            if (isVisited[i]) continue;
+            
+            isVisited[i] = true;
+            generate(depth + 1, piece * 10 + numbers.charAt(i) - '0', numbers, isVisited, digitSet);
+            isVisited[i] = false;
+        }
+    }
+    
+    static int countPrimeNumbers(Set<Integer> digitSet) {
+        boolean[] isNotPrime = new boolean[MAX_NUMBER + 1];
+        int cnt = 0;
+        
+        isNotPrime[0] = isNotPrime[1] = true;
+        for (int i = 2; i <= Math.sqrt(MAX_NUMBER); i++) {
+            if (isNotPrime[i]) continue;
+            
+            for (int j = i * i; j <= MAX_NUMBER; j += i) {
+                isNotPrime[j] = true;
+            }
+        }
+        
+        for (int digit : digitSet) {
+            if (!isNotPrime[digit]) cnt++;
+        }
+        
+        return cnt;
+    }
+    
+    public int solution(String numbers) {
+        Set<Integer> digitSet = new HashSet<>();
+        generate(0, 0, numbers, new boolean[numbers.length() + 1], digitSet);
+        
+        return countPrimeNumbers(digitSet);
+    }
+}
