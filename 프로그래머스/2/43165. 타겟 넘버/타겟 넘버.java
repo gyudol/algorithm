adcde45a90bce18eb257a59f6e6cb0fdd247c2ea
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-06-19 23:26:10 +0900

    [level 2] Title: 타겟 넘버, Time: 5.21 ms, Memory: 86.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,17 @@
+class Solution {
+    static int countTicketNumber(int depth, int sum, int[] numbers, int target) {
+        if (depth == numbers.length) {
+            if (sum == target) return 1;
+            return 0;
+        }
+        
+        int cnt = 0, cur = numbers[depth];
+        
+        return cnt += countTicketNumber(depth + 1, sum - cur, numbers, target) +
+            countTicketNumber(depth + 1, sum + cur, numbers, target);
+    }
+    
+    public int solution(int[] numbers, int target) {
+        return countTicketNumber(0, 0, numbers, target);
+    }
+}
