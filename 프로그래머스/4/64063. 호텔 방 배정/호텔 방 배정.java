a8123b11063df1a6111f3ff93c073b85f604ac2a
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-07-01 18:00:10 +0900

    [level 4] Title: 호텔 방 배정, Time: 397.93 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -2,48 +2,33 @@ import java.util.Map;
 import java.util.HashMap;
 
 class Solution {
+    Map<Long, Long> terminal;
+    
+    long root(long node) {
+        if (!terminal.containsKey(node)) return node;
+        
+        long root = root(terminal.get(node));
+        
+        terminal.put(node, root);
+        return root;
+    }
+    
     public long[] solution(long k, long[] room_number) {
-        Map<Long, Room> hotel = new HashMap<>();
+        terminal = new HashMap<>();
         long[] selected = new long[room_number.length];
         
         for (int i = 0; i < room_number.length; i++) {
-            long num = room_number[i];
+            long room = room_number[i];
             
-            if (hotel.containsKey(num)) {
-                num = hotel.get(num).root().next;
+            if (terminal.containsKey(room)) {
+                room = selected[i] = root(room);
+            } else {
+                selected[i] = room;
             }
             
-            Room room = new Room(num);
-            hotel.put(num, room);
-            selected[i] = num;
-            
-            if (hotel.containsKey(num - 1)) room.merge(hotel.get(num - 1));
-            if (hotel.containsKey(num + 1)) room.merge(hotel.get(num + 1));
+            terminal.put(room, root(room + 1));
         }
         
         return selected;
-    }
-    
-    class Room {
-        Room parent;
-        long next;
-        
-        Room(long num) {
-            this.parent = null;
-            this.next = num + 1;
-        }
-        
-        Room root() {
-            if (parent == null) return this;
-            return parent = parent.root();
-        }
-        
-        void merge(Room v) {
-            Room rootU = root(), rootV = v.root();
-            
-            if (rootU == rootV) return;
-            rootV.parent = rootU;
-            rootU.next = rootV.next = Math.max(rootU.next, rootV.next);
-        }
     }
 }
