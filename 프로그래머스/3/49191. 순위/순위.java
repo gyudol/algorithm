d3570822afb9a9c659eb3dd3ffd382fbc9e0441f
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-07-02 23:56:49 +0900

    [level 3] Title: 순위, Time: 11.81 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,39 +1,42 @@
-import java.util.ArrayList;
-
 class Solution {
-    int dfs(int src, int depth, boolean[] isVisited, ArrayList<ArrayList<Integer>> matches) {
-        if (isVisited[src]) return 0;
-        
-        isVisited[src] = true;
+    int countBackward(int des, boolean[] isVisited, boolean[][] graph) {
         int cnt = 1;
         
-        for (int target : matches.get(src)) {
-            cnt += dfs(target, depth + 1, isVisited, matches);
+        for (int src = 1; src < isVisited.length; src++) {
+            if (!graph[src][des] || isVisited[src]) continue;
+            
+            isVisited[src] = true;
+            cnt += countBackward(src, isVisited, graph);
+        }
+        
+        return cnt;
+    }
+    
+    int countForward(int src, boolean[] isVisited, boolean[][] graph) {
+        int cnt = 1;
+        
+        for (int des = 1; des < isVisited.length; des++) {
+            if (!graph[src][des] || isVisited[des]) continue;
+            
+            isVisited[des] = true;
+            cnt += countForward(des, isVisited, graph);
         }
         
         return cnt;
     }
     
     public int solution(int n, int[][] results) {
-        ArrayList<ArrayList<Integer>> winMatches = new ArrayList<>();
-        ArrayList<ArrayList<Integer>> loseMatches = new ArrayList<>();
+        boolean[][] graph = new boolean[n + 1][n + 1];
         
-        for (int i = 0; i <= n; i++) {
-            winMatches.add(new ArrayList<>());
-            loseMatches.add(new ArrayList<>());
-        }
         for (int[] result : results) {
-            winMatches.get(result[0]).add(result[1]);
-            loseMatches.get(result[1]).add(result[0]);
+            graph[result[0]][result[1]] = true;
         }
         
         int cnt = 0;
         
         for (int i = 1; i <= n; i++) {
-            int winCnt = dfs(i, 0, new boolean[n + 1], winMatches),
-                loseCnt = dfs(i, 0, new boolean[n + 1], loseMatches);
-            
-            if (winCnt + loseCnt - 1 == n) cnt++;
+            if (countForward(i, new boolean[n + 1], graph) + 
+                countBackward(i, new boolean[n + 1], graph) - 1 == n) cnt++;
         }
         
         return cnt;
