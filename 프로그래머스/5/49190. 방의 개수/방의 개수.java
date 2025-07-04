6e7f7d974693de4dac7a8c0d8d939f25dc4d6607
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-07-04 15:12:42 +0900

    [level 5] Title: 방의 개수, Time: 182.92 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.java
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.java
@@ -8,29 +8,27 @@ class Solution {
                                 {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}};
     
     public int solution(int[] arrows) {
-        Map<String, Vertex> vertices = new HashMap<>();
+        Map<Vertex, Set<Vertex>> vertices = new HashMap<>();
         Vertex cur = new Vertex(0, 0);
         int cnt = 0;
         
-        vertices.put(cur.coord, cur);
+        vertices.put(cur, new HashSet<>());
         
         for (int arrow : arrows) {
             // 각 방향마다 2번 이동 (대각선 교차점에서 생기는 방을 감지)
             for (int i = 0; i < 2; i++) {
                 int nx = cur.x + DIRECTIONS[arrow][0],
                     ny = cur.y + DIRECTIONS[arrow][1];
-                String coord = Vertex.getCoord(nx, ny);
+                Vertex next = new Vertex(nx, ny);
 
-                if (!vertices.containsKey(coord)) {
-                    vertices.put(coord, new Vertex(nx, ny));
-                } else if (!cur.connectedVertices.contains(coord)) {
+                if (!vertices.containsKey(next)) {
+                    vertices.put(next, new HashSet<>());
+                } else if (!vertices.get(next).contains(cur)) {
                     cnt++;
                 }
 
-                Vertex next = vertices.get(coord);
-
-                cur.connectedVertices.add(coord);
-                next.connectedVertices.add(cur.coord);
+                vertices.get(cur).add(next);
+                vertices.get(next).add(cur);
                 cur = next;
             }
         }
@@ -38,20 +36,25 @@ class Solution {
         return cnt;
     }
     
-    static class Vertex {
+    class Vertex {
         int x, y;
-        String coord;
-        Set<String> connectedVertices;
         
         Vertex(int x, int y) {
             this.x = x;
             this.y = y;
-            this.coord = getCoord(x, y);
-            this.connectedVertices = new HashSet<>();
         }
         
-        static String getCoord(int x, int y) {
-            return String.format("%d,%d", x, y);
+        @Override
+        public int hashCode() {
+            return (x + "," + y).hashCode();
+        }
+        
+        @Override
+        public boolean equals(Object o) {
+            if (! (o instanceof Object)) return false;
+            Vertex v = (Vertex) o;
+            
+            return x == v.x && y == v.y;
         }
     }
 }
