8a5585bb189df7881ced8479a9a035f71c93ff6f
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-07-09 17:59:12 +0900

    [level 3] Title: 정수 삼각형, Time: 11.55 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[][] triangle) {
+        final int N = triangle.length;
+        int[][] dp = new int[N + 1][N + 1];
+        
+        for (int row = 1; row <= N; row++) {
+            for (int col = 1; col <= row; col++) {
+                dp[row][col] = triangle[row - 1][col - 1] + 
+                    Math.max(dp[row - 1][col], dp[row - 1][col - 1]);
+            }
+        }
+        
+        int max = 0;
+        
+        for (int col = 1; col <= N; col++) {
+            max = Math.max(dp[N][col], max);
+        }
+        
+        return max;
+    }
+}
