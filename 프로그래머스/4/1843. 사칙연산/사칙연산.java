a27a09fb41481f1cd79452eaddc0d31c53012280
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-07-15 00:13:46 +0900

    [level 4] Title: 사칙연산, Time: 63.94 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -16,18 +16,20 @@ class Solution {
     int[][][] dp;
     
     int compute(int start, int end, int compType, String[] arr) {
-        if (dp[compType][start][end] != INITIAL_VALUES[compType])
-            return dp[compType][start][end];
-        if (end - start == 1) return dp[compType][start][end]
-                = Integer.parseInt(arr[start]);
+        if (dp[start][end][compType] != INITIAL_VALUES[compType]) {
+            return dp[start][end][compType];
+        }
+        if (end - start == 1) {
+            return dp[start][end][compType] = Integer.parseInt(arr[start]);
+        }
         
         int result = INITIAL_VALUES[compType];
         
         for (int i = start + 1; i < end; i += 2) {
-            int left = compute(start, i, compType, arr),
-                right = arr[i].equals("+") 
-                    ? compute(i + 1, end, compType, arr)
-                    : compute(i + 1, end, 1 - compType, arr);
+            int left = compute(start, i, compType, arr);
+            int right = arr[i].equals("+") 
+                ? compute(i + 1, end, compType, arr)
+                : compute(i + 1, end, 1 - compType, arr);
             
             int value = arr[i].equals("+") ? left + right : left - right;
             
@@ -36,15 +38,17 @@ class Solution {
             }
         }
         
-        return dp[compType][start][end] = result;
+        return dp[start][end][compType] = result;
     }
     
     public int solution(String arr[]) {
-        dp = new int[2][arr.length][arr.length + 1];
+        dp = new int[arr.length][arr.length + 1][2];
         
-        for (int i = 0; i < arr.length; i++) {
-            Arrays.fill(dp[0][i], Integer.MIN_VALUE);
-            Arrays.fill(dp[1][i], Integer.MAX_VALUE);
+        for (int src = 0; src < arr.length; src++) {
+            for (int des = 0; des <= arr.length; des++) {
+                dp[src][des][0] = INITIAL_VALUES[0];
+                dp[src][des][1] = INITIAL_VALUES[1];
+            }
         }
         
         return compute(0, arr.length, 0, arr);
