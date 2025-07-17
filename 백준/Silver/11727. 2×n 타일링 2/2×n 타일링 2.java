87010c89fbb0ba1b335c72c411f80978d144d866
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-07-17 22:32:21 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 68 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,18 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main {
-	private static final int MOD = 10007;
+class Main {
+	static final int MOD = 10_007;
 	
-	public static void main(String [] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		int [] dp = new int [n + 1];
-		dp[1] = 1;
-		if(n > 1) dp[2] = 3;
+		final int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
 		
-		for(int i = 3; i <= n; i++) dp[i] = (dp[i - 1] + dp[i - 2] * 2) % MOD;
+		dp[0] = dp[1] = 1;
 		
-		System.out.print(dp[n]);	
+		for (int i = 2; i <= N; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2] * 2) % MOD;
+		}
+		
+		System.out.print(dp[N]);
 	}
 }
