778ce0efdebd8afedfe5df28b8f00d46b02d3b95
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-07-18 09:38:36 +0900

    [Silver III] Title: 1로 만들기, Time: 92 ms, Memory: 15488 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,28 +2,24 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-	private static final int TARGET = 1;
-	private static int [] dp;
-	
-	private static int calculate(int N) {
-		if(N <= TARGET) return 0;
-		if(dp[N] != 0) return dp[N];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		final int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
 		
-		dp[N] = calculate(N - 1) + 1;
+		dp[1] = 0;
 		
-		for(int i = 3; i >= 2; i--) {
-			if(N % i == 0) dp[N] = Math.min(dp[N], calculate(N / i) + 1);
+		for (int i = 2; i <= N; i++) {
+			dp[i] = dp[i - 1] + 1;
+			
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+			}
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+			}
 		}
 		
-		return dp[N];
-	}
-	
-	public static void main(String [] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		dp = new int [N + 1];
-		
-		for(int i = 2; i <= N; i++) calculate(i);	// depth가 너무 깊어지면 비효율적이므로 dp를 미리 심어줌
-		System.out.print(calculate(N));
+		System.out.print(dp[N]);
 	}
 }
