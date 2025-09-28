64cdac16475e7a2f39ef95ed110e1a4896addd5d
Gyuchan Kim <83599750+gyudol@users.noreply.github.com>
2025-09-28 17:54:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 무선 충전, Time: 83 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
+++ SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
@@ -4,33 +4,19 @@ class Solution {
 	
 	static Coord A, B;
 	static Charger[] chargers;
-	static int[][] board;
-	
-	static int getChargeAmount(Coord coord, int excluded) {
-		int max = 0;
-		
-		for (int i = 0; i < chargers.length; i++) {
-			if (i != excluded && (board[coord.row][coord.col] & 1 << i) != 0) {
-				max = Math.max(chargers[i].perf, max);
-			}
-		}
-		
-		return max;
-	}
 	
 	static int getMaxChargeAmount() {
 		int max = 0;
 		
-		if (board[A.row][A.col] > 0 && board[B.row][B.col] > 0) {
-			for (int i = 0; i < chargers.length; i++) {
-				if ((board[A.row][A.col] & 1 << i) != 0) {
-					max = Math.max(chargers[i].perf + getChargeAmount(B, i), max);
-				}
+		for (int i = 0; i < chargers.length; i++) {
+			for (int j = 0; j < chargers.length; j++) {
+				int chargeA = chargers[i].isInRange(A.row, A.col) ? chargers[i].perf : 0,
+						chargeB = chargers[j].isInRange(B.row, B.col) ? chargers[j].perf : 0;
+				
+				int total = (i == j && chargeA > 0 && chargeB > 0) ? chargeA : chargeA + chargeB;
+				
+				max = Math.max(total, max);
 			}
-		} else if (board[A.row][A.col] > 0) {
-			max = getChargeAmount(A, -1);
-		} else {
-			max = getChargeAmount(B, -1);
 		}
 		
 		return max;
@@ -69,21 +55,6 @@ class Solution {
     			chargers[i] = new Charger(readInt(), readInt(), readInt(), readInt());
     		}
     		
-    		board = new int[N + 1][N + 1];
-    		
-    		for (int row = 1; row <= N; row++) {
-    			for (int col = 1; col <= N; col++) {
-    				for (int i = 0; i < K; i++) {
-    					int dist = Math.abs(chargers[i].row - row) + 
-    							Math.abs(chargers[i].col - col);
-    					
-    					if (dist <= chargers[i].coverage) {
-    						board[row][col] |= 1 << i;
-    					}
-    				}
-    			}
-    		}
-    		
     		int power = getMaxChargeAmount();
     		
     		for (int i = 0; i < M; i++) {
@@ -120,6 +91,11 @@ class Solution {
     		super(col, row);
     		this.coverage = coverage;
     		this.perf = perf;
+    	}
+    	
+    	boolean isInRange(int row, int col) {
+    		return coverage >= 
+    				Math.abs(row - this.row) + Math.abs(col - this.col);
     	}
     }
 }
